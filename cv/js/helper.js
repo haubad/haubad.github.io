51a0d9106a2f76bdef12fee2073aef5dba9e1df8
haubad <hauvn@yahoo.com>
2015-02-25 16:06:00 +0100

    #map

--- cv/js/helper.js
+++ cv/js/helper.js
@@ -132,7 +132,7 @@ function initializeMap() {
   about a single location.
   */
   function createMapMarker(placeData) {
-    console.log(placeData);
+    //console.log(placeData);
     // The next lines save location data from the search result object to local variables
     var lat = placeData.geometry.location.lat();  // latitude from the place service
     var lon = placeData.geometry.location.lng();  // longitude from the place service
