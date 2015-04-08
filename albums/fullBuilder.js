9322243dd3dc7e6246ec91ca362af587091109c0
haubad <hauvn@yahoo.com>
2015-04-08 10:19:19 +0200

    look 2

--- albums/fullBuilder.js
+++ albums/fullBuilder.js
@@ -1,43 +1,39 @@
 
 var header = $(".full-header");
-var padding = 25;
-var headerHeight = header[0].clientHeight + 2*padding;
-
-// get overlay=
-var overlayStr = getUrlParameter("overlay");
-var overlay = overlayStr == null || overlayStr === "0" ? 1 : 1;
+var margin = 25;
+var headerHeight = header[0].clientHeight;
+var topHeight = headerHeight /*header size*/ + 5/*border-top*/;
 
 // set <div style="height: h">
 //var HTMLpic = '<figure style="background-image: url(\'%data%\');" id="placeholder"></figure>';
-var HTMLpic = '<img src="%data%" id="placeholder">';
-var h = window.innerHeight - headerHeight*overlay /*header size*/ - 5/*border-top*/ - 0 /*border pic*/;
+var h = window.innerHeight - topHeight;
 var pic = $("#pic");
-pic.attr("style", "height: " + h + "px; top: " + (headerHeight*overlay-padding-5) + "px;" + (overlay===0 ? "":" position: relative;"));
+pic.attr("style", "height: " + h + "px;");
 
 // image maps
-$('#zones').attr("style", "height: " + h + "px; width: 100%; position: absolute; top: 0; left: 0; opacity: 0; z-index: 1;");
+$('#zones').attr("style", "height: " + h + "px; width: 100%; position: absolute; left: 0; opacity: 0; z-index: 1; top: " + topHeight + "px;");
 var map = $('#area-prev');
-map.attr('coords', "0, " + (5+headerHeight) + ", " + (window.innerWidth/2) + ", " + (window.innerHeight));
+map.attr('coords', "0, " + topHeight + ", " + (window.innerWidth/2) + ", " + window.innerHeight);
 map.click(function(e) {
     'use strict';
     clickPrev();
 });
 
 map = $('#area-next');
-map.attr('coords', (window.innerWidth/2) + ", " + (5+headerHeight) + ", " + (window.innerWidth) + ", " + (window.innerHeight));
+map.attr('coords', (window.innerWidth/2) + ", " + topHeight + ", " + window.innerWidth + ", " + window.innerHeight);
 map.click(function(e) {
     'use strict';
     clickNext();
 });
 
 function init() {
-    h = window.innerHeight - headerHeight*overlay /*header size*/ - 5/*border-top*/ - 0 /*border pic*/;
+    var h = window.innerHeight - topHeight;
     var pic = $("#pic");
-    pic.attr("style", "height: " + h + "px; top: " + (headerHeight*overlay-padding-5) + "px;" + (overlay===0 ? "":" position: relative;"));
+    pic.attr("style", "height: " + h + "px;");
     
-    $('#zones').attr("style", "height: " + h + "px; width: 100%; position: absolute; top: 0; left: 0; opacity: 0; z-index: 1;");
-    $('#area-prev').attr('coords', "0, " + (5+headerHeight) + ", " + (window.innerWidth/2) + ", " + (window.innerHeight));
-    $('#area-next').attr('coords', (window.innerWidth/2) + ", " + (5+headerHeight) + ", " + (window.innerWidth) + ", " + (window.innerHeight));
+    $('#zones').attr("style", "height: " + h + "px; width: 100%; position: absolute; left: 0; opacity: 0; z-index: 1; top: " + topHeight + "px;");
+    $('#area-prev').attr('coords', "0, " + topHeight + ", " + (window.innerWidth/2) + ", " + window.innerHeight);
+    $('#area-next').attr('coords', (window.innerWidth/2) + ", " + topHeight + ", " + window.innerWidth + ", " + window.innerHeight);
     
     display(json["pics"][currentIdx].url, true);
 }
@@ -49,7 +45,6 @@ var PATH = getUrlParameter("folder");
 PATH = (PATH == null || PATH==="" || PATH==="/" || PATH==="." || PATH==="./" ? "." : PATH);
 
 // set the href of the close button
-//$(".btn-round a").attr("href", "javascript:history.back()");
 $(".btn-round").click(function() {
     history.back();
 });
@@ -69,17 +64,19 @@ $.ajax({
 });
 
 function display(param, anim) {
+
     if (json == null) return;
 
     $("#title-id").html("<span class='violet-text'>" + param + "</span> @" + (currentIdx + 1) + "/" + len);
 	
-	var formattedPic = HTMLpic.replace("%data%", PATH + "/" + param);
-    //pic.fadeOut();
-    //pic.hide();
+    var h = window.innerHeight - topHeight;
     var p = pic.find("#placeholder");    
-    p.attr("style", "height: " + h + "px;");
+    if (window.innerHeight < window.innerWidth) {
+        p.attr("style", "height: " + (h - 2*margin - 5) + "px;");
+    } else {
+        p.attr("style", "width: 98%;");
+    }
     p.attr("src", PATH + "/" + param);
-	pic.fadeIn();
 }
 
 $(".prev").click(function() {
@@ -104,11 +101,11 @@ $('#pic').on('wheel', function(e) {
 
 $(document).keyup(function(e) {
     'use strict';
-    if (e.keyCode===27) { // ESCAPE
+    if (e.keyCode === 27) { // ESCAPE
         history.back();
-    } else if (e.keyCode===39) { // RIGHT
+    } else if (e.keyCode === 39) { // RIGHT
         clickNext();
-    } else if (e.keyCode===37) { // LEFT
+    } else if (e.keyCode === 37) { // LEFT
         clickPrev();
     }
 });
